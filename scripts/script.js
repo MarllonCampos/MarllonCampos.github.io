3e6bb934b7665a797e7f28c4b5f7a75901e54404
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2022-11-04 16:50:25 -0300

    feat: ended portfolio

--- scripts/script.js
+++ scripts/script.js
@@ -7,10 +7,59 @@ document.addEventListener('DOMContentLoaded', async () => {
         document.querySelector('#tecnologias .row').appendChild(createTecnology(tecnology))
     })
 
+    config.projects.forEach((tecnology) => {
+        document.querySelector('#projetos .row').appendChild(createProject(tecnology))
+
+    })
+
 })
 
 
+function createProject(project) {
 
+    const projectCard = document.createElement('a')
+    projectCard.classList.add('project-card')
+    projectCard.setAttribute('href', project.githubLink)
+    projectCard.setAttribute('target', '_blank')
+
+    const projectImg = document.createElement('img')
+    projectImg.setAttribute('src', project.image.url)
+    projectImg.setAttribute('alt', project.image.alt)
+
+    const projectInfo = document.createElement('div')
+    projectInfo.classList.add('project-info')
+
+    const projectName = document.createElement('strong')
+    projectName.classList.add('project-name')
+    projectName.innerText = project.name
+
+    const techsChipsContainer = document.createElement('div')
+    techsChipsContainer.classList.add('techs')
+
+
+    for (let chip of project.chips) {
+        techsChipsContainer.appendChild(createTechChips(chip))
+
+    }
+
+
+    projectInfo.appendChild(projectName)
+    projectInfo.appendChild(techsChipsContainer)
+
+    projectCard.appendChild(projectImg)
+    projectCard.appendChild(projectInfo)
+
+    return projectCard
+}
+
+function createTechChips(chipName) {
+    const chip = document.createElement('span')
+    chip.classList.add('tech-chip')
+
+    chip.innerText = chipName
+
+    return chip
+}
 
 function createTecnology(tecnology) {
 
