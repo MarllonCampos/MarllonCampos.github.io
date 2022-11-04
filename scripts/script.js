d1d3f29d16eabff6a0c71ecfbc3a8e9eb39ebd82
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2022-11-04 16:18:18 -0300

    feat: add tecnologies icons and create a script to render it on screen

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,32 @@
-const ul = document.getElementsByTagName("ul")[1];
-
-async function getRepos() {
-    const response = await fetch('https://api.github.com/users/marlloncampos/repos')
-    const repos = await response.json()
-
-    return repos
-}
+document.addEventListener('DOMContentLoaded', async () => {
+    const response = await fetch('/scripts/config.json')
+    const config = await response.json()
 
 
-showInfo()
-
-async function showInfo() {
-    let data
-    data = await getRepos()
-
-    data.forEach(element => {
-        var li = document.createElement('li')
-        var p = document.createElement('p')
-        var anotherP = document.createElement('p')
-        li.innerHTML = `<strong>* ${element.full_name}</strong>`
-        if (element.description) {
-            anotherP.innerHTML = `<strong>Descrição: </strong>${element.description}`
-        }
-        p.innerHTML = `<a href="${element.html_url}" target="_blank">${element.name} </a>`
-        anotherP.classList.add('descricao')
-        li.appendChild(anotherP)
-        li.appendChild(p)
-        ul.appendChild(li)
-
+    config.tecnologies.forEach((tecnology) => {
+        document.querySelector('#tecnologias .row').appendChild(createTecnology(tecnology))
     })
+
+})
+
+
+
+
+function createTecnology(tecnology) {
+
+    const tecnologyCard = document.createElement('div')
+    tecnologyCard.classList.add('tecnology-card')
+
+    const tecnologyImg = document.createElement('img')
+    tecnologyImg.setAttribute('src', tecnology.image.url)
+    tecnologyImg.setAttribute('alt', tecnology.image.alt)
+
+    const tecnologyName = document.createElement('p')
+    tecnologyName.innerText = tecnology.name
+
+
+    tecnologyCard.appendChild(tecnologyImg)
+    tecnologyCard.appendChild(tecnologyName)
+
+    return tecnologyCard
 }
